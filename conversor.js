a4388ae54356fdfe557ce597c7d6bea50eb5c5ef
Felipe Teixeira <80359142+Feppeli@users.noreply.github.com>
2021-09-22 15:18:17 -0300

    Update conversor.js

--- conversor.js
+++ conversor.js
@@ -1,13 +1,15 @@
+// Função linkada do HTML
 function Converter() {
-    var valorElemento = document.getElementById("valor");
-    var valor = valorElemento.value;
-    var valorEmDolarNumerico = parseFloat(valor);
-    var valorEmReal = valorEmDolarNumerico * 5.29;
+    var valorElemento = document.getElementById("valor"); // Variável que recolhe os dados do input (id="valor") do HTML
+    var valor = valorElemento.value; // retirando apenas o value da variável valorElemento
+    var valorEmDolarNumerico = parseFloat(valor); // Fazendo a variável adotar a tipagem Float
+    var valorEmReal = valorEmDolarNumerico * 5.29; //Criando uma variável com calculo de Dolar para Real
+    //OBS: O valor em Dollar é variável e esse simples código não acompanha essa variação
   
     console.log(valorEmReal);
   
-    var elementoValorConvertido = document.getElementById("valorConvertido");
-    var valorConvertido = "O resultado em real é: R$ " + valorEmReal;
-    elementoValorConvertido.innerHTML = valorConvertido;
+    var elementoValorConvertido = document.getElementById("valorConvertido"); // Retirando os dados de "valorConvertido"
+    var valorConvertido = "O resultado em real é: R$ " + valorEmReal; // Contatenando o valor em real com o texto
+    elementoValorConvertido.innerHTML = valorConvertido; // imprimindo no html o valor já convertido na tela
   }
   
